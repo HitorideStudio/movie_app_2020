4df4ef450f778c6316c7f271db15b273b1c7a679
kunhoon_studio <kunhoon@kunhoon-studioui-MacBookPro.local>
2020-03-20 18:38:36 +0900

    Updates

---
+++ precache-manifest.086ed28bfa1a59443c91151a59c99138.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23918ae4f8cf964db60e8661be18bc4b",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "ed2d791707264c78a1d5",
+    "url": "/movie_app_2020/static/css/main.494dac2e.chunk.css"
+  },
+  {
+    "revision": "ee5f40fffa95eb75bff2",
+    "url": "/movie_app_2020/static/js/2.41708d2b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app_2020/static/js/2.41708d2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed2d791707264c78a1d5",
+    "url": "/movie_app_2020/static/js/main.8ae5b015.chunk.js"
+  },
+  {
+    "revision": "5b9de689d76c950937df",
+    "url": "/movie_app_2020/static/js/runtime-main.db2dcdc9.js"
+  }
+]);
